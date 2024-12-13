09883b8ed8963db48fd6b7a9cc13a457590a7ca3
Akshitha <142525636+akahitha@users.noreply.github.com>
2024-12-13 19:44:31 +0530

    initial commit

---
+++ creatingdb.java
@@ -0,0 +1,25 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class creatingdb {
+
+	public static void main(String[] args) {
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","root");
+			String sql="create database employee";
+			PreparedStatement pmst=conn.prepareStatement(sql);
+			pmst.executeUpdate();
+			conn.close();
+			pmst.close();
+			System.out.println("created");
+		} catch (Exception e) {
+	       System.out.println("error");
+		}
+		
+	}
+}
